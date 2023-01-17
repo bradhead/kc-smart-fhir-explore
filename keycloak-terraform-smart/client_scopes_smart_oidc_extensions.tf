2fd1ae6fc3c4d41be8851f1bacfba050baeb6eeb
Brad Head <38706615+bradhead@users.noreply.github.com>
2023-01-17 15:42:53 -0800

    renamed folders

--- keycloak-terraform-smart/client_scopes_smart_oidc_extensions.tf
+++ keycloak-terraform-smart/client_scopes_smart_oidc_extensions.tf
@@ -14,23 +14,7 @@ resource "keycloak_openid_audience_protocol_mapper" "fhir_user_audience_mapper" 
 
   included_custom_audience = var.keycloak_environment.custom_audience
 }
-/*
-resource "keycloak_generic_protocol_mapper" "oidc-patient-prefix-usermodel-attribute-mapper" {
-  realm_id        = keycloak_openid_client_scope.fhir_user_scope.realm_id
-  client_scope_id = keycloak_openid_client_scope.fhir_user_scope.id
-  name            = "fhirUser Mapper"
-  protocol        = "openid-connect"
-  protocol_mapper = "oidc-patient-prefix-usermodel-attribute-mapper"
-  config = {
-    "user.attribute"       = "resourceId",
-    "claim.name"           = "fhirUser",
-    "jsonType.label"       = "String",
-    "id.token.claim"       = "true",
-    "access.token.claim"   = "false",
-    "userinfo.token.claim" = "true"
-  }
-}
-*/
+
 resource "keycloak_openid_client_scope" "launch_patient_context_scope" {
   realm_id               = data.keycloak_realm.smart_realm.id
   name                   = "launch/patient"
