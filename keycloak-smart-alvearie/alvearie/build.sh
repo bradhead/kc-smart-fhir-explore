32747df0102226f93db9354c54b0d396395b0003
Brad Head <38706615+bradhead@users.noreply.github.com>
2023-01-23 14:17:30 -0800

    legacy build working with Alvearie modules loaded.

--- keycloak-smart-alvearie/alvearie/build.sh
+++ keycloak-smart-alvearie/alvearie/build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+#
+# Builds the Alvearie Keycloak Extensions to support SMART on FHIR.
+# 
+# See the Alvearie Github repository's README.md for more details.
+#
 mkdir -p build
 mkdir -p target
-mkdir -p target/providers
 cd build
-#git clone https://github.com/Alvearie/keycloak-extensions-for-fhir.git
-#cd keycloak-extensions-for-fhir
-git clone https://github.com/XcrigX/keycloak-extensions-for-fhir.git
+git clone https://github.com/Alvearie/keycloak-extensions-for-fhir.git
 cd keycloak-extensions-for-fhir
 git checkout -b feature/quarkus-update
 mvn -B clean package -DskipTests
-cp keycloak-extensions/target/keycloak-extensions-0.5.0-SNAPSHOT.jar ../../target/providers/keycloak-extensions-0.5.0-SNAPSHOT.jar
-cp jboss-fhir-provider/target/jboss-fhir-provider-0.5.0-SNAPSHOT.jar ../../target/providers/jboss-fhir-provider-0.5.0-SNAPSHOT.jar
-cp jboss-fhir-provider/target/jboss-fhir-provider-0.5.0-SNAPSHOT-jboss-modules.jar ../../target/providers/jboss-fhir-provider-0.5.0-SNAPSHOT-jboss-modules.jar
+cp keycloak-extensions/target/keycloak-extensions-0.5.0-SNAPSHOT.jar ../../target/keycloak-extensions-0.5.0-SNAPSHOT.jar
+cp jboss-fhir-provider/target/jboss-fhir-provider-0.5.0-SNAPSHOT.jar ../../target/jboss-fhir-provider-0.5.0-SNAPSHOT.jar
+cp -r jboss-fhir-provider/target/jboss-modules ../../target/
 echo 'Done building Alvearie JAR files for Keycloak SMART on FHIR'
