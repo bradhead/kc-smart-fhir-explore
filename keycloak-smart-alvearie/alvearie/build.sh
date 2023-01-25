47f0d2e84cd3825242d905d557b951d0ede42c51
Brad Head <38706615+bradhead@users.noreply.github.com>
2023-01-24 17:24:12 -0800

    updated build for extensions

--- keycloak-smart-alvearie/alvearie/build.sh
+++ keycloak-smart-alvearie/alvearie/build.sh
@@ -7,11 +7,12 @@
 mkdir -p build
 mkdir -p target
 cd build
-git clone https://github.com/Alvearie/keycloak-extensions-for-fhir.git
+#git clone https://github.com/Alvearie/keycloak-extensions-for-fhir.git
+git clone https://github.com/XcrigX/keycloak-extensions-for-fhir.git
 cd keycloak-extensions-for-fhir
 git checkout -b feature/quarkus-update
 mvn -B clean package -DskipTests
 cp keycloak-extensions/target/keycloak-extensions-0.5.0-SNAPSHOT.jar ../../target/keycloak-extensions-0.5.0-SNAPSHOT.jar
-cp jboss-fhir-provider/target/jboss-fhir-provider-0.5.0-SNAPSHOT.jar ../../target/jboss-fhir-provider-0.5.0-SNAPSHOT.jar
+#cp jboss-fhir-provider/target/jboss-fhir-provider-0.5.0-SNAPSHOT.jar ../../target/jboss-fhir-provider-0.5.0-SNAPSHOT.jar
 cp -r jboss-fhir-provider/target/jboss-modules ../../target/
 echo 'Done building Alvearie JAR files for Keycloak SMART on FHIR'
